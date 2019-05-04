88f96ebe445f90fa33c6ef3e0d40ef4c07e01137
prashantramangupta <you@example.com>
2019-05-05 01:43:09 +0530

    code to measure the progress of repositories in github

---
+++ build.sh
@@ -0,0 +1,2 @@
+rm github_traffic.zip
+zip -r github_metrics.zip * -x \*test_env\*
---
+++ common/constant.py
@@ -0,0 +1,21 @@
+NETWORKS = {
+    999: {
+        'name': 'default',
+		'db': {'DB_HOST': '',
+               'DB_USER': '',
+               'DB_PASSWORD': '',
+               'DB_NAME': '',
+               'DB_PORT': 3306
+               }
+    }
+}
+SLACK_HOOK = {
+    'hostname' : '',
+    'port': 443,
+    'path': '',
+    'method': 'POST',
+    'headers': {
+        'Content-Type': 'application/json'
+    }
+}
+GITHUB_NET_ID = 999
